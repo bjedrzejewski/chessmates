708d36ea70543f504236d4eb9c598c1c0a98aa56
Jan Akerman <j.f.akerman@gmail.com>
2017-04-19 14:37:15 +0100

    Schedules data updates from Lichess REST API.
    At start up and every hour following, the scheduler will
    run to update our current lichess objects.
    
    Interesting things to note are that we still have our cache
    which is set to expire every 30 minutes. This should still
    result in us getting clean data every time we run the
    scheduled task.

---
+++ src/main/java/com/chessmates/lichess/data/LichessDataScheduler.groovy
@@ -0,0 +1,37 @@
+package com.chessmates.lichess.data
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+/**
+ * Scheduled tasks that hit the Lichess API for data.
+ */
+@Component
+class LichessDataScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(LichessDataScheduler)
+
+    LichessDataService lichessDataService
+
+    @Autowired
+    LichessDataScheduler(LichessDataService lichessDataService) {
+        this.lichessDataService = lichessDataService
+    }
+
+    @Scheduled(initialDelay = 0L, fixedDelay = 3600000L)
+    void updateData() {
+        final startTime = new Date()
+        logger.debug "Starting Lichess data update: ${startTime} (${startTime.getTime()})"
+
+        // TODO: Rename these functions, they are saving as a side affect and that isn't clear.
+        final  players = lichessDataService.getPlayers()
+        lichessDataService.getGames(players)
+
+        final endTime = new Date()
+        logger.debug "Finished Lichess data update: ${endTime} (${endTime.getTime()})"
+    }
+
+}
