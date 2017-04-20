a08bb0401f06eafd89d3c75e4d75eb4c790ae370
Jan Akerman <j.f.akerman@gmail.com>
2017-04-20 12:39:40 +0100

    Scheduled tasks no long run in unit tests.
    spring-boot-starter-actuator enables scheduling application wide.
    This cause me headaches here as I was looking at having a different
    configuration for our unit tests that simply turned off scheduling.
    
    Instead, I've used a conditional inclusion on the scheduling bean itself.
    It's enabled in the main applicaiton properties and disabled in the test's.
    
    I've addeda comment to the enable scheduling annotation so I hope that
    helps future me, or future team members.

--- src/main/java/com/chessmates/lichess/data/LichessDataScheduler.groovy
+++ src/main/java/com/chessmates/lichess/data/LichessDataScheduler.groovy
@@ -3,6 +3,7 @@ package com.chessmates.lichess.data
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component
  * Scheduled tasks that hit the Lichess API for data.
  */
 @Component
+@ConditionalOnProperty(value = 'chessmates.lichess.api.dataupdate', havingValue = 'true', matchIfMissing = false)
 class LichessDataScheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(LichessDataScheduler)
