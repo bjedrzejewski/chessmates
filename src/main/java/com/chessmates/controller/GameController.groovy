7e976b4af929d136d8827081edb026fcda4e76e5
Jan Akerman <j.f.akerman@gmail.com>
2017-04-20 16:07:58 +0100

    Adds getPlayer & getGame endpoint
    I haven't added any tests here as theres no real logic. I've tested
    the fetching of teams from the LichessDataService - that's the
    real meat of the application so far. Is it worth adding in e2e tests
    that mock out the repositories and hit the REST end points? Does
    that add any real value? Opinions please!

--- src/main/java/com/chessmates/controller/GameController.groovy
+++ src/main/java/com/chessmates/controller/GameController.groovy
@@ -4,28 +4,31 @@ import com.chessmates.service.EntityService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 /**
- * Controller for returning information about Lichess Entities.
+ * Controller for returning information about team Lichess games.
  */
 @RestController
 @CrossOrigin
-class EntityController {
+class GameController {
 
     @Autowired
     EntityService entityService
-
-    /**
-     * Return a list of all team players
-     */
-    @GetMapping(value = 'players')
-    List getPlayers() { entityService.getPlayers() }
 
     /**
      * Return a list of all unique games played between all team members.
      */
     @GetMapping(value = 'games')
     List getGames() { entityService.getGames() }
+
+    /**
+     * Return a game of a given ID.
+     */
+    @GetMapping(value = 'game/{id}')
+    getGame(@PathVariable('id') String id) {
+        entityService.getGame(id)
+    }
 
 }
--- src/main/java/com/chessmates/service/EntityService.groovy
+++ src/main/java/com/chessmates/service/EntityService.groovy
@@ -10,8 +10,18 @@ interface EntityService {
     List getPlayers()
 
     /**
+     * Return a player of a given ID.
+     */
+    def getPlayer(String id)
+
+    /**
      * Returns a list of games played between team players.
      */
     List getGames()
+
+    /**
+     * Return a game of a given ID.
+     */
+    def getGame(String id)
 
 }
